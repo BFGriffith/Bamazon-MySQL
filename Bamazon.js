fadc81cd4137f9e60690c3d0d876bfdfe38c1443
BFGriffith <benjaminforbesgriffith@gmail.com>
2016-06-17 21:18:48 -0400

    Bamazon_db ten-products and Bamazon.js connector+console.log products

--- Bamazon.js
+++ Bamazon.js
@@ -1,2 +1,22 @@
 var mysql = require('mysql');
 var prompt = require('prompt');
+
+var connector = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: ""
+});
+
+connector.connect(function(err) {
+  if(err){
+    console.log('error connecting to Bamazon_db');
+    return;
+  }
+  console.log('Connection to Bamazon database has been successfully established.');
+});
+
+connector.query('SELECT * FROM Bamazon_db.products',function(err, rows) {
+  if(err) throw err;
+  console.log('Data received from “Bamazon” database:\n');
+  console.log(rows);
+});
--- Bamazon_db.sql
+++ Bamazon_db.sql
@@ -4,7 +4,29 @@ create table products (
 	  itemID integer NOT NULL auto_increment,
     productName varchar(60),
     departmentName varchar(50),
-    price float(99,2),
+    price float(62,2),
     stockQuantity integer NOT NULL,
     constraint PRIMARY KEY (itemID)
 );
+
+-- products:
+INSERT INTO	products (productName, departmentName, price, stockQuantity)
+	values ('lightsaber', 'weapons', 99.99, 12);
+INSERT INTO	products (productName, departmentName, price, stockQuantity)
+	values ('hoverboard', 'vehicles', 999.99, 100);
+INSERT INTO	products (productName, departmentName, price, stockQuantity)
+	values ('Gungnir', 'weapons', 10000.00, 1);
+INSERT INTO	products (productName, departmentName, price, stockQuantity)
+	values ('Fantasticar', 'vehicles', 100000.00, 1);
+INSERT INTO	products (productName, departmentName, price, stockQuantity)
+	values ('Trident of Neptune', 'weapons', 900000.00, 1);
+INSERT INTO	products (productName, departmentName, price, stockQuantity)
+	values ('Morgul-blades',  'weapons', 9.99, 9);
+INSERT INTO	products (productName, departmentName, price, stockQuantity)
+ 	values ('Batmobile', 'vehicles', 500000.00, 17);
+INSERT INTO	products (productName, departmentName, price, stockQuantity)
+	values ('Settlers of Catan', 'games', 39.97, 75);
+INSERT INTO	products (productName, departmentName, price, stockQuantity)
+	values ('Crabs Adjust Humidity', 'games', 12.87, 19);
+INSERT INTO	products (productName, departmentName, price, stockQuantity)
+	values ('Utter Nonsense', 'games', 21.02, 30);
